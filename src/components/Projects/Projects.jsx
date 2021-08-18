3d2612c4074befd12c1428544e5ab6eb41561ae9
Anchit Mishra <champu.1729@icloud.com>
2021-08-18 14:53:04 +0800

    Added AI/Machine Learning related projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,8 @@ import haptiverseImage from '../../images/photo_haptiverse.jpg'
 import bigTwoImage from '../../images/photo_bigtwo.jpg'
 import astridCrisisImage from '../../images/photo_astrid.jpg'
 import blockchainImage from '../../images/photo_blockchain.png'
+import atariPhoto from '../../images/photo_atari.png'
+import onionsPhoto from '../../images/photo_onions.png'
 import './Projects.css'
 
 export default function Projects(props) {
@@ -42,8 +44,50 @@ export default function Projects(props) {
                     <br />
                     <br />
                     <Heading marginLeft={5} textAlign='left' size='sm'>
+                        <a href='https://github.com/anchit1729/Transfer-Reinforcement-Learning'>
+                            2. Transfer Reinforcement Learning - An Atari Experiment
+                        </a>
+                    </Heading>
+                    <br />
+                    <Text marginLeft={5} textAlign='left' maxWidth={600} fontWeight='medium' size='xl' color='black'>
+                        This project is an implementation of a DQN model used to train on the Atari environments in Python.
+                        It uses the Dueling architecture with Experience Replay in order to obtain noticeable improvements over the vanilla
+                        DQN architecture. In particular, this project was an exploratory attempt to see if transfer learning techniques
+                        used in traditional Deep Learning can translate directly to Reinforcement Learning or not (hint: they cannot). In short,
+                        the conclusion from the project was that domain transfer is a hard task to perform due to a significant amount of negative transfer,
+                        i.e. learning features that are specifically seen in the original environment, and thus have to be un-learnt before the model can
+                        pick up new knowledge from the new domain. Despite the seemingly letdown conclusions, it was an enjoyable project to pursue
+                        and taught me more about analysing performance and inferring from observations in the field of AI.
+                    </Text>
+                    <br />
+                    <br />
+                    <Image marginLeft={35} width={500} src={atariPhoto} alt='A screenshot of the Pong environment used to train a reinforcement learning agent' />
+                    <br />
+                    <br />
+                    <br />
+                    <Heading marginLeft={5} textAlign='left' size='sm'>
+                        <a href='https://github.com/anchit1729/CuseHacks2021'>
+                            3. Fried Onions Classifier - A Fun Hackathon Project
+                        </a>
+                    </Heading>
+                    <br />
+                    <Text marginLeft={5} textAlign='left' maxWidth={600} fontWeight='medium' size='xl' color='black'>
+                        This project was a fun little hackathon idea implemented by me during CuseHacks 2021, for which we even won a prize.
+                        The theme of the hackathon was broad in that it allowed participants to work on any problem area for 24 hours. I decided to
+                        work on something related to my experiences during COVID - a machine learning classifier that can help people check whether the
+                        food they've cooked is properly cooked yet or if it still needs to be left on the stove. This proof-of-concept simply implements
+                        a convolutional neural-network trained on a dataset of ~1000 images of onions and is able to classify with 80% accuracy whether or not
+                        the onions have been properly cooked yet.
+                    </Text>
+                    <br />
+                    <br />
+                    <Image marginLeft={35} width={500} src={onionsPhoto} alt='A screenshot of the fried onion classifier prototype' />
+                    <br />
+                    <br />
+                    <br />
+                    <Heading marginLeft={5} textAlign='left' size='sm'>
                         <a href='https://github.com/anchit1729/distributed-ledger-raft-consensus'>
-                            2. Distributed Ledger Prototype - Raft Distributed Consensus
+                            4. Distributed Ledger Prototype - Raft Distributed Consensus
                         </a>
                     </Heading>
                     <br />
@@ -61,7 +105,7 @@ export default function Projects(props) {
                     <br />
                     <Heading marginLeft={5} textAlign='left' size='sm'>
                         <a href='https://github.com/anchit1729/Big-Two-Java'>
-                            3. Big Two
+                            5. Big Two
                         </a>
                     </Heading>
                     <br />
@@ -79,7 +123,7 @@ export default function Projects(props) {
                     <br />
                     <Heading marginLeft={5} textAlign='left' size='sm'>
                         <a href='https://github.com/anchit1729/Text-based-Adventure'>
-                            4. Astrid Crisis
+                            6. Astrid Crisis
                         </a>
                     </Heading>
                     <br />
@@ -97,6 +141,6 @@ export default function Projects(props) {
                     <br />
                 </div>
             </div>
-        </Center>
+        </Center >
     )
 }
